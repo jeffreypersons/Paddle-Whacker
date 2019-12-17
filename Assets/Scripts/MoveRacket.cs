4afd70b4f7ac2ad241955f4bdec43e73a60b0e79
Jeffrey Persons <jperman8@gmail.com>
2019-12-16 18:36:40 -0800

    Add user input to control rackets
    As the first step in user input (and later, for player2's AI) - we add vertical movement control for the rackets.
    
    In upcoming iterations we will specify different scripts for each racket, but as a first step this will suffice.

--- Assets/Scripts/MoveRacket.cs
+++ Assets/Scripts/MoveRacket.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class MoveRacket : MonoBehaviour
 {
+    public float speed = 30;
     void FixedUpdate()
     {
-
+        float v = Input.GetAxisRaw("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
     }
 }
