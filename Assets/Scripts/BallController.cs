2b8dc8161febebe77bac18e5912020be0a186bcb
Jeffrey Persons <jperman8@gmail.com>
2019-12-17 17:04:40 -0800

    Added basic controllers for ball and rackets
    Can now move the left racket, and the ball bounces between, but no angles are yet applied, so it always moves horizontally.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public readonly float SPEED = 30;
+    public readonly Vector2 INITIAL_DIRECTION = Vector2.right;
+    private Rigidbody2D ball;
+    
+    void Start()
+    {
+        ball = GetComponent<Rigidbody2D>();
+        ball.velocity = INITIAL_DIRECTION * SPEED;
+    }
+
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public readonly float SPEED = 30;
+    private Rigidbody2D racket;
+    private Rigidbody2D ball; // change this later, its disgusting. this class doesn't own that object...
+
+    void Start()
+    {
+        racket = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Rigidbody2D>();
+        racket.velocity = new Vector2(0, SPEED);
+    }
+    void FixedUpdate()
+    {
+        float input = Input.GetAxisRaw("Vertical");
+        if (input == 0)
+        {
+            racket.velocity = Vector2.zero;
+        }
+        else
+        {
+            Vector2 directionToMove = input > 0 ? Vector2.up : Vector2.down;
+            racket.velocity = directionToMove * SPEED;
+        }
+    }
+}
