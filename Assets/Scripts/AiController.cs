12673aa9d0d9282b304e6aae55d11a69a2de8733
Jeffrey Persons <jperman8@gmail.com>
2019-12-17 17:04:40 -0800

    Added basic controllers for ball and rackets
    Can now move the left racket, and the ball bounces between, but no angles are yet applied, so it always moves horizontally.

---
+++ Assets/Scripts/AiController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class AiController : MonoBehaviour
+{
+    public readonly float SPEED = 30;
+
+    private Rigidbody2D racket;
+    private Rigidbody2D ball; // change this later, its disgusting. this class doesn't own that object...
+
+    void Start()
+    {
+        racket = GameObject.FindGameObjectWithTag("Ai").GetComponent<Rigidbody2D>();
+        ball = GameObject.FindGameObjectWithTag("Ball").GetComponent<Rigidbody2D>();
+        racket.velocity = new Vector2(0, SPEED);
+    }
+    void FixedUpdate()
+    {
+        if (racket.position.y == ball.position.y)
+        {
+            racket.velocity = new Vector2(0, 0);
+        }
+        else
+        {
+            Vector2 directionToMove = racket.position.y < ball.position.y ? Vector2.up : Vector2.down;
+            racket.velocity = directionToMove * SPEED;
+        }
+    }
+}
