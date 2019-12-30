340b2d80f557e3cd7d00e05b3ec6703875c088ca
Jeffrey Persons <jperman8@gmail.com>
2019-12-30 14:46:01 -0800

    Allow input axis name to be set in inspector

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,20 @@
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
-    
+    public string inputAxisName;
+
     private Rigidbody2D paddle;
     private Vector2 moveDirection;
     
     void Start()
     {
         paddle = GameObject.Find("PlayerPaddle").GetComponent<Rigidbody2D>();
+        paddle.velocity = Vector2.zero;
     }
 
     void Update()
     {
+        Debug.Log(Input.GetAxisRaw(inputAxisName));
         moveDirection = new Vector2(0, moveSpeed * Mathf.Sign(Input.GetAxisRaw("Vertical")));
     }
 
