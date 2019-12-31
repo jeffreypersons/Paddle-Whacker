6679f7ba9dbf82f172e957892eb136b7664c6f1a
Jeffrey Persons <jperman8@gmail.com>
2019-12-30 17:43:28 -0800

    Remove need for caching ball movement direction

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -5,27 +5,25 @@ public class BallController : MonoBehaviour
     public float moveSpeed;
     public Vector2 initialDirection;
 
-    private Vector2 moveDirection;
     private Rigidbody2D ball;
 
     void Start()
     {
         ball = GetComponent<Rigidbody2D>();
-        moveDirection = initialDirection;
-        ball.velocity = moveSpeed * moveDirection;
+        ball.velocity = moveSpeed * initialDirection;
     }
-    
+
     // upon hitting a paddle, we invert the direction of the ball
-    // TODO: add checking for walls
-    // TODO: add handling for hitting top of paddle
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Paddle"))
         {
-            float invertedXDirection = moveDirection.x > 0 ? -1 : 1;
-            float offsetFromPaddleCenterToBall = (ball.position.y - collision.transform.position.y) / collision.collider.bounds.size.y;
-            moveDirection = new Vector2(invertedXDirection, offsetFromPaddleCenterToBall).normalized;
-            ball.velocity = moveSpeed * moveDirection;
+            Vector2 paddlePosition = collision.rigidbody.position;
+            Vector2 paddleSize = collision.collider.bounds.size;
+
+            float invertedXDirection = ball.position.x - paddlePosition.x > 0 ? -1 : 1;
+            float offsetFromPaddleCenterToBall = (ball.position.y - paddlePosition.y) / paddleSize.y;
+            ball.velocity = moveSpeed * new Vector2(invertedXDirection, offsetFromPaddleCenterToBall).normalized;
         }
     }
-}
+} 
