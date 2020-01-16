2f5d8b0285a9c92929907b8a734cf71863879a32
Jeffrey Persons <jperman8@gmail.com>
2020-01-15 20:14:58 -0800

    Add switch to game scene on startbutton click

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Control : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
