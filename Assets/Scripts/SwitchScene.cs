a4638cf6a920439f30e56c6a40c327ca355d291d
Jeff Persons <jperman8@gmail.com>
2020-01-15 21:34:55 -0800

    Basic start screen (#19)
    Adds a start (title) screen with a start button, as well as the following support structure:
    * Prefabs for title screen elements (title text, loadscene button, etc)
    * Script to switch loading
    * Game build settings modified to support title screen
    * Restructuring alignment so the game scene matches start screen
    * Prefabs for consistency between the event systems/cameras of the title/game scenes

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+    public void NextScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
