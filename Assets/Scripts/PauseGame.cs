9d8053e957fcd72a665b84e2c4327b03493c88eb
Jeffrey Persons <jperman8@gmail.com>
2020-01-16 18:51:27 -0800

    Toggle timescale on pause button press

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+    private bool isPaused;
+
+    void Start()
+    {
+        isPaused = false;
+    }
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+    }
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+}
