47abb297b2c11b9a81936cf824028eb5b18f80dc
Jeffrey Persons <jperman8@gmail.com>
2020-02-04 11:50:07 -0800

    Refactor gameround logic into separate events
    First round of extracting out the logic related to scorekeeping/round-handling/resetting from the ball class

--- Assets/Code/Controllers/GameEvents.cs
+++ Assets/Code/Controllers/GameEvents.cs
@@ -4,7 +4,9 @@ using UnityEngine.Events;
 [System.Serializable]
 public class StringEvent : UnityEvent<string> { }
 
+// note events are triggered and handled programmatically (via listeners and invocations)
 public class GameEvents : MonoBehaviour
 {
     public static StringEvent onPaddleHit = new StringEvent();
+    public static StringEvent onVerticalWallHit = new StringEvent();
 }
