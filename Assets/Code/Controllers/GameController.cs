18286fc087c0b469736e813ff1f5bf2b63b9c798
Jeffrey Persons <jperman8@gmail.com>
2020-02-04 15:11:59 -0800

    Add todo

--- Assets/Code/Controllers/GameController.cs
+++ Assets/Code/Controllers/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : MonoBehaviour
         Debug.Log("Starting Game");
         GameData.Init();
 
+        // todo: reduce the amount of 'magic' strings...
         // todo: add listeners for player scores instead in a separate script or something,
         // so they simply just change when `data.player1Score` and `data.player2Score` change
         ballController = GameObject.Find("Ball").GetComponent<BallController>();
