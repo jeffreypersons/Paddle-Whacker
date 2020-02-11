cb94874c01f4f76e10527b8020a5061f0a03393f
Jeffrey Persons <jperman8@gmail.com>
2020-02-11 08:03:41 -0800

    add todo's

--- Assets/Code/Controllers/AiController.cs
+++ Assets/Code/Controllers/AiController.cs
@@ -87,13 +87,11 @@ public class AiController : MonoBehaviour
 
             positionToMoveTowards = PredictOptimalDefensivePosition();
             Debug.Log("path:[" + string.Join(",", path) + "]");
-            /*
             Debug.Log("pos"        + ball.position                + ", " +
                       "dir"        + ball.velocity.normalized     + ", " +
                       "bounces("   + ((trajectory.Count - 2) / 2) + ")," +
                       "prediction" + positionToMoveTowards +
                       " ===> trajectory:[" + string.Join(",", trajectory) + "]");
-            */
         }
     }
     // note: ball hit position not needed since time/reflections/friction is not yet accounted for
@@ -102,6 +100,7 @@ public class AiController : MonoBehaviour
     {
         path.Clear();
         trajectory.Clear();
+        trajectory.Add(ball.position);
         float targetX = paddle.position.x;
         ComputeTrajectory(ball.position, ball.velocity.normalized, targetX, maxReflectionCount);
         return new Vector2(targetX, trajectory[trajectory.Count - 1].y);
@@ -112,6 +111,8 @@ public class AiController : MonoBehaviour
     // when nothing to bounce off of is hit, let the trajectory go all the way to the paddle
     private void ComputeTrajectory(Vector2 position, Vector2 direction, float targetX, int reflectionsRemaining)
     {
+        // todo: make a trajectory path class with node for each and then use that abstraction and move this stuff to over there...
+        // todo: figure out why it only works for a single bounce!
         if (position.x == targetX)
         {
             trajectory.Add(position);
@@ -119,7 +120,7 @@ public class AiController : MonoBehaviour
         }
         else if (reflectionsRemaining == 0)
         {
-            path.Add("targetX");
+            path.Add("used all reflections");
             float horizontalStepForPartialBounce = 0.5f * Mathf.Abs(targetX - position.x);
             trajectory.Add(position + (direction * horizontalStepForPartialBounce));
             return;
@@ -127,17 +128,18 @@ public class AiController : MonoBehaviour
 
         float distanceToPaddle = Mathf.Abs(targetX - position.x);
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distanceToPaddle);
-        Debug.DrawLine(position, position + (direction * distanceToPaddle), Color.green, 5f);
         if (hit.transform != null && hit.transform.CompareTag("HorizontalWall"))
         {
             path.Add(hit.transform.name);
             trajectory.Add(hit.point);
+            Debug.DrawLine(position, hit.point, Color.green, 2.5f);
             ComputeTrajectory(hit.point, Vector2.Reflect(direction, hit.normal), targetX, reflectionsRemaining - 1);
         }
         else
         {
             path.Add("targetX");
             trajectory.Add(position + (direction * distanceToPaddle));
+            Debug.DrawLine(position, trajectory[trajectory.Count - 1], Color.green, 2.5f);
         }
     }
 }
