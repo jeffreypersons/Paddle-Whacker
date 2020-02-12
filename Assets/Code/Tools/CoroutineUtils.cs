fe9664612951bd40b758e234413bc7524946f5ef
Jeffrey Persons <jperman8@gmail.com>
2020-02-12 14:54:46 -0800

    Make base cases and action use more explicit

---
+++ Assets/Code/Tools/CoroutineUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using UnityEngine;
+
+public static class CoroutineUtils
+{
+    // Wait at least the time delay, where the delay takes effect AFTER executing the task
+    //
+    // example usage:
+    //    StartCoroutine(
+    //       WaitAtLeast(myDelay, () => { /* do stuff */ })
+    //    );
+    public static IEnumerator WaitAtLeast(double timeDelay, Action action)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+        action();
+        stopWatch.Stop();
+
+        double elapsedTime = stopWatch.Elapsed.TotalSeconds;
+        if (elapsedTime < timeDelay)
+        {
+            yield return new WaitForSeconds((float)(timeDelay - elapsedTime));
+        }
+    }
+}
