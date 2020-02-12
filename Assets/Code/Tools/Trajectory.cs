6fce2124ae9754b23e4214cdfc6fbead90af4ef3
Jeffrey Persons <jperman8@gmail.com>
2020-02-12 09:31:23 -0800

    Draft trajectory class

--- Assets/Code/Tools/Trajectory.cs
+++ Assets/Code/Tools/Trajectory.cs
@@ -2,32 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trajectory : MonoBehaviour
+public class Trajectory
 {
-    private Rigidbody2D ball;
-    private List<Vector2> trajectory;
-    private List<string> path;
+    public int maxNumBounces;
 
-    // Start is called before the first frame update
-    void Start()
+    private int numInUse;
+
+    public struct Node
     {
-
+        public Vector2 position;
+        public Vector2 direction;
+        public string contact;
     }
 
+    private Node[] path;
+
+    Trajectory()
+    {
+        numInUse = 0;
+        path = new Node[maxNumBounces];
+        for (int i = 0; i < maxNumBounces; i++)
+        {
+            path[i] = new Node();
+        }
+    }
+    private void AddPoint(Vector2 position, Vector2 direction, string contact)
+    {
+        if (numInUse + 1 < maxNumBounces)
+        {
+            path[numInUse].position  = position;
+            path[numInUse].direction = direction;
+            path[numInUse].contact   = contact;
+            numInUse++;
+        }
+    }
+    public void ComputeNewTrajectory(Vector2 startPosition, Vector2 startDirection, float targetX)
+    {
+        AddPoint(startPosition, startDirection, "start");
+        while ()
+        {
+            SetPoint()
+        }
+        ComputeTrajectory();
+    }
     // conceptually: draw line from ball position until a reflection occurs, repeating until target is met,
     // or no recursion depth (maxReflections) is met (which case it calculates partway off the last bounce)
     // when nothing to bounce off of is hit, let the trajectory go all the way to the paddle
-    private void ComputeTrajectory(Vector2 position, Vector2 direction, float targetX, int reflectionsRemaining)
+    // todo: figure out why it only works for a single bounce!
+    private void ComputeTrajectory(Node node, float targetX, int index)
     {
-        // todo: make a trajectory path class with node for each and then use that abstraction and move this stuff to over there...
-        // todo: figure out why it only works for a single bounce!
-        if (position.x == targetX)
+        if (node.position.x == targetX)
         {
-            trajectory.Add(position);
+            AddPoint(node.position, node.direction, "targetX");
             return;
         }
-        else if (reflectionsRemaining == 0)
+        else if (index == maxNumBounces)
         {
+            AddPoint(node.position, node.direction, "targetX");
             path.Add("used all reflections");
             float horizontalStepForPartialBounce = 0.5f * Mathf.Abs(targetX - position.x);
             trajectory.Add(position + (direction * horizontalStepForPartialBounce));
@@ -50,4 +81,5 @@ public class Trajectory : MonoBehaviour
             Debug.DrawLine(position, trajectory[trajectory.Count - 1], Color.green, 2.5f);
         }
     }
+
 }
