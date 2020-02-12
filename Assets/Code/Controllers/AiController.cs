cb7e4e7ec0f012eff2fa7cfaa61c5c7c3336e03e
Jeffrey Persons <jperman8@gmail.com>
2020-02-11 21:31:51 -0800

    Cleanup debug statements

--- Assets/Code/Controllers/AiController.cs
+++ Assets/Code/Controllers/AiController.cs
@@ -14,20 +14,18 @@ public class AiController : MonoBehaviour
     private int maxReflectionCount = 5;
     private Vector2 positionToMoveTowards;
     private Rigidbody2D ball;
-    private List<Vector2> trajectory;
-    private List<string> path;
+    private List<Vector2> ballTrajectory;
 
     private Vector2 initialPosition;
     private Rigidbody2D paddle;
 
     public AiController()
     {
-        trajectory = new List<Vector2>();
-        path = new List<string>();
+        ballTrajectory = new List<Vector2>();
     }
     public void Reset()
     {
-        trajectory.Clear();
+        ballTrajectory.Clear();
         paddle.position = initialPosition;
         positionToMoveTowards = initialPosition;
         paddle.velocity = Vector2.zero;
@@ -82,28 +80,21 @@ public class AiController : MonoBehaviour
         {
             isTargetPredicted = true;
             isMovementSuspended = true;
+            positionToMoveTowards = PredictOptimalDefensivePosition();
             yield return new WaitForSeconds(responseTime);
             isMovementSuspended = false;
-
-            positionToMoveTowards = PredictOptimalDefensivePosition();
-            Debug.Log("path:[" + string.Join(",", path) + "]");
-            Debug.Log("pos"        + ball.position                + ", " +
-                      "dir"        + ball.velocity.normalized     + ", " +
-                      "bounces("   + ((trajectory.Count - 2) / 2) + ")," +
-                      "prediction" + positionToMoveTowards +
-                      " ===> trajectory:[" + string.Join(",", trajectory) + "]");
+            Debug.Log("Computed trajectory=[" + string.Join(",", ballTrajectory) + "]");
         }
     }
     // note: ball hit position not needed since time/reflections/friction is not yet accounted for
     // note: since x is fixed (currently only vertical paddle movement is possible), we only need to predict optimal y
     private Vector2 PredictOptimalDefensivePosition()
     {
-        path.Clear();
-        trajectory.Clear();
-        trajectory.Add(ball.position);
+        ballTrajectory.Clear();
+        ballTrajectory.Add(ball.position);
         float targetX = paddle.position.x;
         ComputeTrajectory(ball.position, ball.velocity.normalized, targetX, maxReflectionCount);
-        return new Vector2(targetX, trajectory[trajectory.Count - 1].y);
+        return new Vector2(targetX, ballTrajectory[ballTrajectory.Count - 1].y);
     }
 
     // conceptually: draw line from ball position until a reflection occurs, repeating until target is met,
@@ -111,18 +102,17 @@ public class AiController : MonoBehaviour
     // when nothing to bounce off of is hit, let the trajectory go all the way to the paddle
     private void ComputeTrajectory(Vector2 position, Vector2 direction, float targetX, int reflectionsRemaining)
     {
-        // todo: make a trajectory path class with node for each and then use that abstraction and move this stuff to over there...
         // todo: figure out why it only works for a single bounce!
         if (position.x == targetX)
         {
-            trajectory.Add(position);
+            ballTrajectory.Add(position);
             return;
         }
         else if (reflectionsRemaining == 0)
         {
-            path.Add("used all reflections");
+            Debug.Log("ComputeTrajectory::hit=used all reflections");
             float horizontalStepForPartialBounce = 0.5f * Mathf.Abs(targetX - position.x);
-            trajectory.Add(position + (direction * horizontalStepForPartialBounce));
+            ballTrajectory.Add(position + (direction * horizontalStepForPartialBounce));
             return;
         }
 
@@ -130,16 +120,16 @@ public class AiController : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distanceToPaddle);
         if (hit.transform != null && hit.transform.CompareTag("HorizontalWall"))
         {
-            path.Add(hit.transform.name);
-            trajectory.Add(hit.point);
+            Debug.Log("ComputeTrajectory::hit=" + hit.transform.name);
+            ballTrajectory.Add(hit.point);
             Debug.DrawLine(position, hit.point, Color.green, 2.5f);
             ComputeTrajectory(hit.point, Vector2.Reflect(direction, hit.normal), targetX, reflectionsRemaining - 1);
         }
         else
         {
-            path.Add("targetX");
-            trajectory.Add(position + (direction * distanceToPaddle));
-            Debug.DrawLine(position, trajectory[trajectory.Count - 1], Color.green, 2.5f);
+            Debug.Log("ComputeTrajectory::hit=targetX");
+            ballTrajectory.Add(position + (direction * distanceToPaddle));
+            Debug.DrawLine(position, ballTrajectory[ballTrajectory.Count - 1], Color.green, 2.5f);
         }
     }
 }
