a5e99fc0d1eac71507b6b42228326ec2f4d724ae
Jeff Persons <jperman8@gmail.com>
2020-02-12 22:17:59 -0800

    Add ai prediction for player ball trajectory - Part 2 balancing the ai (#38)
    Finally got a more balanced ai that can handle arbitrary number of bounces, and is much cleaner than the previous part 1 implementation, as the details of the trajectory algorithm and coroutines were modularized (in class PredictedTrajectory and CoroutineUtils respectively).
    
    Also, added a not so noticeable speed handicap to the AI, and a response time delay, to make it beatable (hence the balancing improvements). Few setting files changed to support this as a build, to.

---
+++ Assets/Code/Tools/CoroutineUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections;
+using UnityEngine;
+
+public static class CoroutineUtils
+{
+    // Wait at least the time delay, where the delay takes effect AFTER executing the task
+    //
+    // example usage:
+    //    StartCoroutine(
+    //       WaitAtLeast(myDelay, () => { /* do stuff */ })
+    //    );
+    public static IEnumerator RunAfter(float timeDelay, Action action)
+    {
+        yield return new WaitForSeconds(timeDelay);
+        action();
+    }
+}
