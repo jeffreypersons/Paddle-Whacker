e16863295daecebbac1956508360aa5458446396
Jeffrey Persons <jperman8@gmail.com>
2020-03-10 16:01:38 -0700

    Rearrange paddles as childeren of player node with corresponding range colliders

---
+++ Assets/Code/Scripts/ExpandColliderToPaddleMovementRange.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ExpandColliderToPaddleMovementRange : MonoBehaviour
+{
+    public string paddleName;
+    private BoxCollider2D topWall;
+    private BoxCollider2D bottomWall;
+
+    private BoxCollider2D paddle;
+    private BoxCollider2D movementRegion;
+
+    void Start()
+    {
+        topWall = GameObject.Find("TopWall").GetComponent<BoxCollider2D>();
+        bottomWall = GameObject.Find("BottomWall").GetComponent<BoxCollider2D>();
+
+        paddle = GetComponentInParent<BoxCollider2D>();
+        movementRegion = GetComponent<BoxCollider2D>();
+    }
+    void Update()
+    {
+        ComputeMovementRange(
+            new Vector2(paddle.bounds.min.x, bottomWall.bounds.max.y),
+            new Vector2(paddle.bounds.max.x, topWall.bounds.min.y),
+            new Vector2(paddle.transform.localScale.x, 1)
+        );
+    }
+    private void ComputeMovementRange(Vector2 min, Vector2 max, Vector2 scale)
+    {
+        movementRegion.size = scale * (max - min);
+        movementRegion.transform.position = min + (movementRegion.size * 0.5f);
+    }
+
+}
