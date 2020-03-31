27806a98e5213ca1a43a768fec630d9b934df1e2
Jeffrey Persons <jperman8@gmail.com>
2020-03-31 11:23:24 -0700

    Extract adjustable setting panel prefabs for use in mainmenu

---
+++ Assets/Code/Controllers/AdjustSliderSettingController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteAlways]
+public class AdjustSliderSettingController : MonoBehaviour
+{
+    [SerializeField] public string description;
+    [SerializeField] public string numberSuffix;
+    [SerializeField] public string defaultValue;
+    [SerializeField] public string minValue;
+    [SerializeField] public string maxValue;
+
+    public TMPro.TextMeshProUGUI label;
+    public Slider slider;
+
+    void Awake()
+    {
+
+    }
+    void OnEnable()
+    {
+        slider.onValueChanged.AddListener(UpdateLabel);
+    }
+    void OnDisable()
+    {
+        slider.onValueChanged.RemoveListener(UpdateLabel);
+    }
+    public void UpdateLabel(float value)
+    {
+        label.text = $"{description}: {value}{numberSuffix}";
+    }
+
+    private bool TryReadValues(out float val, out float min, out float max)
+    {
+        val = 0.0f; min = 0.0f; max = 0.0f;
+        return float.TryParse(defaultValue, out val) &&
+               float.TryParse(minValue,     out min) &&
+               float.TryParse(maxValue,     out max);
+    }
+
+    void Update()
+    {
+        float val, min, max;
+        if (TryReadValues(out val, out min, out max) && MathUtils.IsWithinRange(val, min, max))
+        {
+            slider.value        = val;
+            slider.minValue     = min;
+            slider.maxValue     = max;
+            slider.wholeNumbers = MathUtils.IsAllInteger(defaultValue, minValue, maxValue);
+        }
+        else
+        {
+            Debug.LogError($"Expected min <= default <= max as all floats or all ints," +
+                           $"recieved {defaultValue}, {minValue}, {maxValue}` instead");
+        }
+    }
+}
--- Assets/Code/Controllers/MainMenuController.cs
+++ Assets/Code/Controllers/MainMenuController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 
@@ -6,31 +7,85 @@ public class MainMenuController : MonoBehaviour
 {
     public GameObject mainMenu;
     public Button startButton;
+    public Button settingsButton;
+    public Button aboutButton;
     public Button quitButton;
 
-    public StartNewGameInfo newGameInfo;
+    public GameObject startPanel;
+    public GameObject settingsPanel;
+    public GameObject aboutPanel;
+
+    private StartNewGameInfo newGameInfo;
+
+    private List<Button> buttonsToHideWhenActive;
+    private List<TMPro.TextMeshProUGUI> labelsToHideWhenActive;
 
     void Awake()
     {
-        newGameInfo = new StartNewGameInfo(3, StartNewGameInfo.Difficulty.Easy);
+        newGameInfo = new StartNewGameInfo(3, StartNewGameInfo.Difficulty.Medium);
+
+        startPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+
+        buttonsToHideWhenActive = GameObjectUtils.FindAllObjectsWithTags<Button>("Button");
+        labelsToHideWhenActive = GameObjectUtils.FindAllObjectsWithTags<TMPro.TextMeshProUGUI>("Label");
     }
 
     void OnEnable()
     {
-        startButton.onClick.AddListener(TriggerLoadMainMenuSceneEvent);
+        startButton.onClick.AddListener(OpenStartPanel);
+        settingsButton.onClick.AddListener(OpenSettingsPanel);
+        aboutButton.onClick.AddListener(OpenAboutPanel);
         quitButton.onClick.AddListener(SceneUtils.QuitGame);
+
+        startPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        aboutPanel.SetActive(false);
     }
     void OnDisable()
     {
-        startButton.onClick.RemoveListener(TriggerLoadMainMenuSceneEvent);
+        startButton.onClick.RemoveListener(OpenStartPanel);
+        settingsButton.onClick.RemoveListener(OpenSettingsPanel);
+        aboutButton.onClick.RemoveListener(OpenAboutPanel);
         quitButton.onClick.RemoveListener(SceneUtils.QuitGame);
     }
 
-    public void TriggerLoadMainMenuSceneEvent()
+    public void OpenStartPanel()
+    {
+        startPanel.SetActive(true);
+        ToggleMainMenuVisibility(true);
+    }
+    public void OpenSettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+        ToggleMainMenuVisibility(true);
+    }
+    public void OpenAboutPanel()
+    {
+        aboutPanel.SetActive(true);
+        ToggleMainMenuVisibility(true);
+    }
+
+    public void TriggerLoadGameEvent()
     {
         SceneUtils.LoadScene("Game", () =>
         {
             GameEventCenter.startNewGame.Trigger(newGameInfo);
         });
+    }
+
+    private void ToggleMainMenuVisibility(bool enableMainMenu)
+    {
+        bool hideBackground = !enableMainMenu;
+        for (int i = 0; i < buttonsToHideWhenActive.Count; i++)
+        {
+            buttonsToHideWhenActive[i].gameObject.SetActive(hideBackground);
+            buttonsToHideWhenActive[i].enabled = hideBackground;
+        }
+        for (int i = 0; i < labelsToHideWhenActive.Count; i++)
+        {
+            labelsToHideWhenActive[i].enabled = hideBackground;
+        }
     }
 }
