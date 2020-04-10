229d854d7ff515d71a202aad5cfe3886f829709d
Jeffrey Persons <jperman8@gmail.com>
2020-04-10 15:49:10 -0700

    Update stub

--- Assets/Code/Controllers/IngameSoundController.cs
+++ Assets/Code/Controllers/IngameSoundController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 
+
 public class IngameSoundController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
+    {
+
+    }
+    void OnEnable()
+    {
+
+    }
+    void OnDisable()
     {
 
     }
