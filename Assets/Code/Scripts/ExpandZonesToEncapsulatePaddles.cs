233fe26ee6f0db8812ab7db086544a87c7b79bb4
Jeffrey Persons <jperman8@gmail.com>
2020-04-22 14:16:30 -0700

    Relocate paddlezones so that their boxcolliders are independent of paddle movement

---
+++ Assets/Code/Scripts/ExpandZonesToEncapsulatePaddles.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+[ExecuteAlways]
+public class ExpandZonesToEncapsulatePaddles : MonoBehaviour
+{
+    [SerializeField] private float extraWidth = default;
+    [SerializeField] private BoxCollider2D leftZone    = default;
+    [SerializeField] private BoxCollider2D rightZone   = default;
+
+    [SerializeField] private BoxCollider2D leftPaddle  = default;
+    [SerializeField] private BoxCollider2D rightPaddle = default;
+    [SerializeField] private BoxCollider2D bottomWall  = default;
+    [SerializeField] private BoxCollider2D topWall     = default;
+
+    private float InnerArenaMaxY  { get { return topWall.bounds.min.y; } }
+    private float InnerArenaMinY  { get { return bottomWall.bounds.max.y; } }
+
+    void Update()
+    {
+        if (!Application.IsPlaying(gameObject))
+        {
+            if (leftPaddle.transform.hasChanged)
+            {
+                ResizeZoneColliderToEncapsulatePaddle(leftZone, leftPaddle);
+            }
+            if (rightPaddle.transform.hasChanged)
+            {
+                ResizeZoneColliderToEncapsulatePaddle(rightZone, rightPaddle);
+            }
+        }
+    }
+    private void ResizeZoneColliderToEncapsulatePaddle(BoxCollider2D zone, BoxCollider2D paddle)
+    {
+        float height = InnerArenaMaxY - InnerArenaMinY;
+        zone.size = new Vector3(paddle.size.x + extraWidth, height);
+        zone.transform.position = new Vector3(paddle.bounds.center.x, InnerArenaMinY + (height * 0.50f));
+    }
+}
