c08316a68a39f48c31b1f3bc31aacdf1cb0fa4ac
Jeffrey Persons <jperman8@gmail.com>
2020-04-23 16:20:47 -0700

    Add tag selection drawer to editor scripts and available attributes

---
+++ Assets/Code/Tools/CollectionUtils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+public static class CollectionUtils
+{
+    public static string[] PrependToArray(string value, string[] source)
+    {
+        string[] newArray = new string[source.Length + 1];
+        newArray[0] = value;
+        Array.Copy(source, 0, newArray, 1, source.Length);
+        return newArray;
+    }
+}
