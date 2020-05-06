0e3915a77c8d1fe018c7013766e0ae15251aeed3
Jeffrey Persons <jperman8@gmail.com>
2020-05-06 10:04:59 -0700

    Adjust misalignment of startNewGame event field

--- Assets/Code/Data/GameEventCenter.cs
+++ Assets/Code/Data/GameEventCenter.cs
@@ -12,7 +12,7 @@ public static class GameEventCenter
     public static GameEvent<RecordedScore> scoreChange                = new GameEvent<RecordedScore>();
     public static GameEvent<PaddleZoneIntersectInfo> zoneIntersection = new GameEvent<PaddleZoneIntersectInfo>();
 
-    public static GameEvent<GameSettingsInfo>     startNewGame        = new GameEvent<GameSettingsInfo>();
+    public static GameEvent<GameSettingsInfo> startNewGame        = new GameEvent<GameSettingsInfo>();
     public static GameEvent<RecordedScore>    pauseGame           = new GameEvent<RecordedScore>();
     public static GameEvent<string>           resumeGame          = new GameEvent<string>();
     public static GameEvent<string>           gotoMainMenu        = new GameEvent<string>();
