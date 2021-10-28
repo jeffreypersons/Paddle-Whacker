f50383e8c0ec70142e914cad7e62e3a56009bbbc
Jeffrey Persons <jperman8@gmail.com>
2021-10-28 13:06:28 -0400

    Improve letterboxing of camera to resize on inspector field changes

--- Assets/Code/Controllers/LetterBoxedCameraController.cs
+++ Assets/Code/Controllers/LetterBoxedCameraController.cs
@@ -16,38 +16,46 @@ public class LetterBoxedCameraController : MonoBehaviour
     private const int DEFAULT_TARGET_WIDTH  = 1920;
     private const int DEFAULT_TARGET_HEIGHT = 1080;
     private readonly Rect FULL_VIEWPORT_RECT = new Rect(0, 0, 1, 1);
+    
+    [SerializeField] [Range(MIN_NUM_PIXELS, MAX_NUM_PIXELS)] private int desiredWidth  = DEFAULT_TARGET_WIDTH;
+    [SerializeField] [Range(MIN_NUM_PIXELS, MAX_NUM_PIXELS)] private int desiredHeight = DEFAULT_TARGET_HEIGHT;
+    
 
-    [SerializeField] [Range(MIN_NUM_PIXELS, MAX_NUM_PIXELS)] private int targetWidth  = DEFAULT_TARGET_WIDTH;
-    [SerializeField] [Range(MIN_NUM_PIXELS, MAX_NUM_PIXELS)] private int targetHeight = DEFAULT_TARGET_HEIGHT;
-
-    private int currentWindowWidth;
-    private int currentWindowHeight;
+    private RenderWindowResolution ActualResolution { get; set; }
+    private RenderWindowResolution TargetResolution { get; set; }
 
     void Update()
     {
-        if (Screen.width != currentWindowWidth || Screen.height != currentWindowHeight)
+        if (!ActualResolution.IsSize(Screen.width, Screen.height) ||
+            !TargetResolution.IsSize(desiredWidth, desiredHeight))
         {
-            currentWindowWidth  = Screen.width;
-            currentWindowHeight = Screen.height;
+            ActualResolution = new RenderWindowResolution(Screen.width, Screen.height);
+            TargetResolution = new RenderWindowResolution(desiredWidth, desiredHeight);
             UpdateLetterbox();
         }
     }
 
     private void UpdateLetterbox()
     {
-        float targetAspect = targetWidth / (float)targetHeight;
-        float windowAspect = currentWindowWidth / (float)currentWindowHeight;
-        float scaleHeight  = windowAspect / targetAspect;
-        
-        Camera attachedCamera = GetComponent<Camera>();
-        attachedCamera.rect = scaleHeight < 1.00f ? GetLetterboxRect(scaleHeight) : GetPillarboxRect(scaleHeight);
-        if (attachedCamera.rect != FULL_VIEWPORT_RECT)
+        float scaledHeight = ActualResolution.Aspect / TargetResolution.Aspect;
+        Rect fittedRect = scaledHeight < 1.00f ? GetLetterboxRect(scaledHeight) : GetPillarboxRect(scaledHeight);
+        if (fittedRect != FULL_VIEWPORT_RECT)
         {
-            Debug.Log($"Current window resolution {currentWindowWidth}x{currentWindowHeight} " +
-                      $"differs in aspect of target {targetWidth}x{targetHeight} - " +
-                      $"adjusted viewport bounds to [{attachedCamera.rect.min}, {attachedCamera.rect.max}]");
+            GetComponent<Camera>().rect = fittedRect;
+            Debug.Log($"Detected mismatch between aspect ratios of " +
+                      $"actual {ActualResolution.Width}x{ActualResolution.Height} and " +
+                      $"desired {TargetResolution.Width}x{TargetResolution.Height} resolutions - "  +
+                      $"adjusted viewport bounds to [{fittedRect.min}, {fittedRect.max}]");
+        }
+        else
+        {
+            Debug.Log($"Matched aspects between " +
+                      $"actual {ActualResolution.Width}x{ActualResolution.Height} and " +
+                      $"desired {TargetResolution.Width}x{TargetResolution.Height} resolutions - "  +
+                      $"no resizing of viewport bounds needed");
         }
     }
+
     private Rect GetLetterboxRect(float scaleHeight)
     {
         return new Rect(0.00f, (1.00f - scaleHeight) * 0.50f, 1.00f, scaleHeight);
@@ -56,5 +64,21 @@ public class LetterBoxedCameraController : MonoBehaviour
     {
         float scalewidth = 1.00f / scaleHeight;
         return new Rect((1.00f - scalewidth) * 0.50f, 0.00f, scalewidth, 1.00f);
+    }
+}
+
+public struct RenderWindowResolution
+{
+    public readonly int   Width;
+    public readonly int   Height;
+    public readonly float Aspect;
+    
+    public bool IsSize(int width, int height) => width == Width && height == Height;
+    public override string ToString() => $"RenderWindowResolution{{size={Width}x{Height},aspect={Aspect}}}";
+    public RenderWindowResolution(int width, int height)
+    {
+        Width  = width;
+        Height = height;
+        Aspect = width / (float)height;
     }
 }
